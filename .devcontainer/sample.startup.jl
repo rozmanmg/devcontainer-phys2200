94d4f5c0a6f019014f3cbaecff3ba16047c54cfc
rozmanmg <164565284+rozmanmg@users.noreply.github.com>
2026-05-05 19:43:53 -0400

    Update Julia environment variables
    Change JULIA_CONDAPKG_BACKEND to 'Null' and set Python and Conda executable paths.

--- .devcontainer/sample.startup.jl
+++ .devcontainer/sample.startup.jl
@@ -1,6 +1,9 @@
 push!(LOAD_PATH, ".")
 
-ENV["JULIA_CONDAPKG_BACKEND"] = "System"
+ENV["JULIA_CONDAPKG_BACKEND"] = "Null"
+ENV["JULIA_PYTHONCALL_EXE"] = "$(ENV["HOME"])/conda/3/x86_64/bin/python"
+ENV["JULIA_CONDAPKG_EXE"] = "$(ENV["HOME"])/conda/3/x86_64/bin/conda"
+
 ENV["JULIA_CONDAPKG_VERBOSITY"] = "-1"
 ENV["JULIA_CONDAPKG_LOG"] = "warn"
 
