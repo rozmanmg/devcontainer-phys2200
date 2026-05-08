faa3b6915d885305b5e701cdf4ecdcd0a5f71f92
rozmanmg <164565284+rozmanmg@users.noreply.github.com>
2026-05-08 15:17:05 -0400

    Set Python executable for Julia Conda package
    Add Python executable path for Julia environment.

--- .devcontainer/sample.startup.jl
+++ .devcontainer/sample.startup.jl
@@ -2,8 +2,9 @@ push!(LOAD_PATH, ".")
 
 # Set environment variables for using PythonPlot
 ENV["JULIA_CONDAPKG_BACKEND"] = "Null"
+ENV["JULIA_PYTHONCALL_EXE"] = "/opt/conda/bin/python"
 # ENV["JULIA_PYTHONCALL_EXE"] = "$(ENV["HOME"])/.julia/conda/3/x86_64/bin/python"
-ENV["JULIA_CONDAPKG_EXE"] = "$(ENV["HOME"])/.julia/conda/3/x86_64/bin/conda"
+# ENV["JULIA_CONDAPKG_EXE"] = "$(ENV["HOME"])/.julia/conda/3/x86_64/bin/conda"
 
 ENV["JULIA_CONDAPKG_VERBOSITY"] = "-1"
 ENV["JULIA_CONDAPKG_LOG"] = "warn"
