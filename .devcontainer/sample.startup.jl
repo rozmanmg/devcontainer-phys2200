b487108618e18d010708e2bcadd1794e4e97047c
rozmanmg <164565284+rozmanmg@users.noreply.github.com>
2026-05-12 14:48:48 -0400

    Clarify PythonPlot environment variable setup
    Updated comments to clarify the use of PythonPlot with shared Python and conda.

--- .devcontainer/sample.startup.jl
+++ .devcontainer/sample.startup.jl
@@ -2,7 +2,7 @@ push!(LOAD_PATH, ".")
 
 ENV["JUPYTER"]="/opt/conda/bin/jupyter-lab"
 
-# Set environment variables for using PythonPlot
+# Set environment variables for using PythonPlot with pre-installed shared python and conda
 ENV["JULIA_CONDAPKG_BACKEND"] = "Null"
 ENV["JULIA_PYTHONCALL_EXE"] = "/opt/conda/bin/python"
 
